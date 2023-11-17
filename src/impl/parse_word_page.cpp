317437e7ebc18987e1cc47f86ee1aad094e519df
eeriemyxi <eeriemyxi@proton.me>
2023-11-17 18:17:57 +0530

    feat(word parser): added crucial data as members of the class

--- src/impl/parse_word_page.cpp
+++ src/impl/parse_word_page.cpp
@@ -19,9 +19,11 @@ std::string support_lang_enum_to_name(SupportedLanguage lang){
     return "English";
 }
 
-ParseWordPage::ParseWordPage(const std::string& word, const std::string& html, const SupportedLanguage& lang) {
-    lxb_status_t status;
-
+ParseWordPage::ParseWordPage(
+        const std::string& word,
+        const std::string& html,
+        const SupportedLanguage& lang
+    ) {
     ParseWordPage::word = word;
     LOG_F(INFO, "Parsing word page of word \"%s\".", word.data());
     ParseWordPage::html = html;
@@ -29,48 +31,63 @@ ParseWordPage::ParseWordPage(const std::string& word, const std::string& html, c
     std::string lang_name = support_lang_enum_to_name(lang);
     LOG_F(INFO, "Page Language: %s.", lang_name.data());
 
-    auto html_parser = lxb_html_parser_create();
-    status = lxb_html_parser_init(html_parser);
+    ParseWordPage::html_parser = lxb_html_parser_create();
+    lxb_status_t status = lxb_html_parser_init(ParseWordPage::html_parser);
     if (status != LXB_STATUS_OK) {
         throw AppException(201, "Couldn't init html_parser.");
     }
 
-    auto css_parser = lxb_css_parser_create();
-    status = lxb_css_parser_init(css_parser, NULL);
+    ParseWordPage::css_parser = lxb_css_parser_create();
+    status = lxb_css_parser_init(ParseWordPage::css_parser, NULL);
     if (status != LXB_STATUS_OK) {
         throw AppException(202, "Couldn't init css_parser.");
     }
 
-    auto selectors = lxb_selectors_create();
-    status = lxb_selectors_init(selectors);
+    ParseWordPage::selectors = lxb_selectors_create();
+    status = lxb_selectors_init(ParseWordPage::selectors);
     if (status!= LXB_STATUS_OK) {
         throw AppException(203, "Couldn't init css selectors.");
     }
 
-    auto selectors_list = lxb_css_selectors_parse(css_parser, (lxb_char_t*) ("#" + lang_name).data(), lang_name.length() + 1);
-    if (css_parser->status != LXB_STATUS_OK) {
+    ParseWordPage::selector_list = lxb_css_selectors_parse(
+            ParseWordPage::css_parser, 
+            (lxb_char_t*) ("#" + lang_name).data(),
+            lang_name.length() + 1
+    );
+    if (ParseWordPage::css_parser->status != LXB_STATUS_OK) {
         throw AppException(204, "Couldn't parse for selector list.");
     }
 
-    auto document = lxb_html_parse(html_parser, (lxb_char_t*) html.data(), html.size());
-    auto body_node = lxb_dom_interface_node(document->body);
+    ParseWordPage::document = lxb_html_parse(ParseWordPage::html_parser,
+            (lxb_char_t*) html.data(),
+            html.size()
+    );
+    ParseWordPage::body_node = lxb_dom_interface_node(ParseWordPage::document->body);
 
-    auto english_collection = lxb_dom_collection_make(&document->dom_document, 128);
+    ParseWordPage::english_collection = lxb_dom_collection_make(
+            &ParseWordPage::document->dom_document, 128
+    );
 
-    status = lxb_selectors_find(
-            selectors,
-            body_node,
-            selectors_list,
+}
+
+void ParseWordPage::get_data() {
+    std::cout << "Hello world!\n\t- from the parser." << '\n';
+    auto status = lxb_selectors_find(
+            this->selectors,
+            this->body_node,
+            this->selector_list,
             [](lxb_dom_node_t *node, lxb_css_selector_specificity_t spec, void *ctx) -> lxb_status_t {
-                lxb_html_serialize_cb(
-                    node->parent->next->next->next->next, 
-                    [](const lxb_char_t *data, size_t len, void *ctx) -> lxb_status_t {
-                        std::cout << data;
-                        return LXB_STATUS_OK;
-                    },
-                    NULL
-                );
-                std::cout << '\n';
+                // lxb_html_serialize_cb(
+                //     node->parent->next->next->next->next,
+                //     [](const lxb_char_t *data, size_t len, void *ctx) -> lxb_status_t {
+                //         std::cout << data;
+                //         return LXB_STATUS_OK;
+                //     },
+                //     NULL
+                // );
+                // std::cout << '\n';
+
+                
                 return LXB_STATUS_OK;
             },
             NULL
@@ -78,8 +95,4 @@ ParseWordPage::ParseWordPage(const std::string& word, const std::string& html, c
     if (status != LXB_STATUS_OK) {
         throw AppException(205, "Couldn't find selector.");
     }
-}
-
-void ParseWordPage::get_data() {
-    std::cout << "Hello world!\n\t- from the parser." << '\n';
 }
--- src/proto/parse_word_page.h
+++ src/proto/parse_word_page.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <lexbor/dom/dom.h>
+#include <lexbor/css/css.h>
+#include <lexbor/selectors/selectors.h>
+#include <lexbor/html/html.h>
 
 #include "../proto/wiktionary.h"
 
@@ -11,6 +14,15 @@ class ParseWordPage {
     public:
         std::string word;
         std::string html;
+        
+        lxb_html_parser_t* html_parser; 
+        lxb_css_parser_t* css_parser; 
+        lxb_selectors_t* selectors; 
+        lxb_css_selector_list_t* selector_list;
+
+        lxb_html_document_t* document;
+        lxb_dom_node_t* body_node;
+        lxb_dom_collection_t* english_collection; 
 
         ParseWordPage(const std::string& word, const std::string& html, const SupportedLanguage& lang);
         void get_data();
